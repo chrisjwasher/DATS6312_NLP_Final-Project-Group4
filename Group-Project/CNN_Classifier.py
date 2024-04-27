989cd950563d14bd8d122ee2b76bc02eea248b79
chrisjwasher <112447869+chrisjwasher@users.noreply.github.com>
2024-04-27 14:03:37 -0400

    added read_json

--- Group-Project/CNN_Classifier.py
+++ Group-Project/CNN_Classifier.py
@@ -3,6 +3,7 @@ import json
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from read_json import read_json_folder
 
 from gensim.parsing.preprocessing import remove_stopwords
 import gensim.downloader as api
@@ -33,44 +34,14 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Device available for running: ")
 print(device)
 
-def read_json_folder(folder_path):
-    """
-    Read JSON files from a folder and return a list of dictionaries.
-    Args:
-        folder_path (str): Path to the folder containing JSON files.
-    Returns:
-        list: A list of dictionaries containing data from each JSON file.
-    """
-    json_data_list = []
-
-    # Check if the folder path exists
-    if not os.path.exists(folder_path):
-        print(f"Folder '{folder_path}' does not exist.")
-        return json_data_list
-
-    # Iterate over files in the folder
-    for filename in os.listdir(folder_path):
-        file_path = os.path.join(folder_path, filename)
-        # Check if the file is a JSON file
-        if filename.endswith('.json'):
-            with open(file_path, 'r') as f:
-                # Load JSON data from the file
-                try:
-                    json_data = json.load(f)
-                    json_data_list.append(json_data)
-                except json.JSONDecodeError:
-                    print(f"Error reading JSON from file: {file_path}")
-                    continue
-
-    df = pd.DataFrame.from_dict(json_data_list)
-
-    return df, json_data_list
 
 print("Reading data...")
 df, json_data_list = read_json_folder('../data/jsons')
 df['full_content'] = df['title'] + ' ' + df['content']
 df = df.drop(['topic', 'source', 'url', 'date', 'authors','title', 'content',
               'content_original', 'source_url', 'bias_text','ID'], axis=1)
+
+
 #------------------------------------------------------------------------------------
 #
 # Text preprocessing and data loading
